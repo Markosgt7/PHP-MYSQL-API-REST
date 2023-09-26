aaa4a0c1d72c5c0b524d872805362b9cd63e5e46
Markosgt7 <markos.guate7@gmail.com>
2023-09-26 12:52:17 -0600

    se agregan los status code para las peticiones de tipo http

--- controller/categoria.php
+++ controller/categoria.php
@@ -8,30 +8,91 @@
   $body = json_decode(file_get_contents("php://input"),true);
 
   switch ($_GET["op"]) {
+    //obtener todas las categorias
     case "GetAll":
       $datos = $categoria->get_categoria();
-      echo json_encode($datos);
+      if($datos){
+        http_response_code(200);//
+        echo json_encode($datos);
+      }else{
+        http_response_code(404);//No encontrado
+        echo json_encode("No se encontraron categorias");
+      }
       break;
+    //obtener categorias por id  
     case "GetId":
+      if(!isset($body["cat_id"])){
+        http_response_code(400);//solicitud incorrecta
+        echo json_encode('Parametro cat_id es obligatorio');
+        break;
+      }
       $datos = $categoria->get_categoria_por_id($body["cat_id"]);
-      echo json_encode($datos);
+      if($datos){
+        http_response_code(200);
+        echo json_encode($datos);
+      }else{
+        http_response_code(404);//Not found
+        echo json_encode('Categoría no existe');
+      }
       break;
+    //insertar categoria  
     case "insert":
+      if(!isset($body["cat_nom"]) || !isset($body["cat_obs"])){
+        http_response_code(400);
+        echo json_encode('Parametros cat_nom y cat_obs son obligatorios');
+        break;
+      }
+
       $datos = $categoria->insert_categoria($body["cat_nom"],$body["cat_obs"]);
-      echo json_encode("Ingreso Exitoso");
+      if($datos){
+        http_response_code(201); //creado correctamente
+        echo json_encode("Ingreso Exitoso");
+      }else{
+        http_response_code(500); //error server
+        echo json_encode("Error al insertar la categoría");
+      }
       break;
+
+      //actualizar categoria
     case "update":
+      if(!isset($body["cat_id"]) || !isset($body["cat_nom"]) || !isset($body["cat_obs"])){
+        http_response_code(400);
+        echo json_encode('Parámetros cat_nom, cat_obs son obligatorios.');
+        break;
+      }
+
       $datos = $categoria->update_categoria($body["cat_id"],$body["cat_nom"],$body["cat_obs"]);
-      echo json_encode("Actualizacion Exitosa");
+      if($datos){
+        http_response_code(200);
+        echo json_encode("Actualización exitosa");
+      }else{
+        http_response_code(500);
+        echo json_encode("Error al actualizar Categoría");
+      }
       break;
+    //eliminar categorias  
     case "delete":
+      if(!isset($body["cat_id"])){
+        http_response_code(400);//solicitud incorrecta
+        echo json_encode('parametro cat_id es obligatorio');
+        break;
+      }
+      
       $datos = $categoria->delete_categoria($body["cat_id"]);
-      echo json_encode("eliminación Exitosa");
+      if($datos){
+        http_response_code(200);
+        echo json_encode("eliminación Exitosa");
+      }else{
+        http_response_code(404);//Not Found
+        echo json_encode('No se encontró el registro');
+      }
       break;
-    
+    //mensaje por defecto
     default:
-      echo json_encode("Está opción no esta disponible consulta documentación");
+      http_response_code(400); 
+      echo json_encode( "Opción no válida");
       break;
+      
   }
 
 ?>
